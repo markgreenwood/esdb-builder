3b9a7bb2711f7c39984d7e338e996953a57beacf
Mark Greenwood <mark.greenwood@cascadeenergy.com>
2017-07-10 09:40:17 -0700

    Abstracted some details to be specified with CL args; read data and index settings from JSON

--- scripts/esdb-builder.js
+++ scripts/esdb-builder.js
@@ -1,24 +1,22 @@
 const config = require('config');
 const R = require('ramda');
 const es = require('elasticsearch');
+const commandLineArgs = require('command-line-args');
 
-// TODO: Genericize where we get index attributes
-const indexSettings = require('../index-templates/user');
+const argOptions = [
+  { name: 'dry-run', alias: 'd', type: Boolean, required: false },
+  { name: 'verbose', alias: 'v', type: Boolean, required: false },
+  { name: 'quiet', alias: 'q', type: Boolean, required: false },
+  { name: 'indexname', alias: 'i', type: String, multiple: false, required: true }
+];
+const args = commandLineArgs(argOptions);
 
 const esClient = new es.Client(R.pick(['host'], config.get('elasticsearch')));
 
-// TODO: Create indexNames from cmd-line args
-const indexName = {
-  dev: 'user-dev',
-  test: 'user-test'
-};
+const indexName = R.propOr('', 'indexname', args);
+const esIndex = indexName === '' ? null : R.concat(indexName, '-' + process.env.NODE_ENV);
 
-const esIndex = indexName[process.env.NODE_ENV];
-
-if (!esIndex) {
-  console.log('NODE_ENV should be either \'test\' or \'dev\'');
-  process.exit(0);
-}
+console.log(`Creating index ${esIndex}`);
 
 // TODO: What happens if item.id is undefined/null?
 const bulkRequestBuilder = type =>
@@ -29,54 +27,11 @@ const bulkRequestBuilder = type =>
     )
   );
 
-// TODO: Read data from JSON file specified thru cmd-line args
-const testUsers = [
-  {
-    userId: 1,
-    username: 'mark.greenwood',
-    email: 'mark@email.com',
-    createdBy: '1001490',
-    createdTimestamp: '2017-07-07T22:11:38.145Z',
-    firstName: 'Mark',
-    lastName: 'Greenwood',
-    jobTitle: 'Software Engineer',
-    location: 'PDX',
-    notes: '',
-    theme: 'sensei',
-    license: {
-      name: 'sensei',
-      version: '',
-      accepted: true
-    },
-    phoneNumber: 'xxx.xxx.xxxx',
-    userLevel: 0,
-    modifiedTimestamp: '2017-07-07T22:11:38.148Z'
-  },
-  {
-    userId: 2,
-    username: 'star.lord',
-    email: 'peterq@gotg.net',
-    createdBy: '1001490',
-    createdTimestamp: '2017-07-10T22:11:38.145Z',
-    firstName: 'Peter',
-    lastName: 'Quill',
-    jobTitle: 'Scrum Master',
-    location: 'Terra',
-    notes: '',
-    theme: 'sensei',
-    license: {
-      name: 'sensei',
-      version: '',
-      accepted: true
-    },
-    phoneNumber: 'xxx.xxx.xxxx',
-    userLevel: 0,
-    modifiedTimestamp: '2017-07-10T22:11:38.148Z'
-  }
-];
+const data = require(`./data/${indexName}`);
+const indexSettings = require(`./index-templates/${indexName}`);
 
 // TODO: abstract 'type' here
-const bulkRequest = bulkRequestBuilder('user')(testUsers);
+const bulkRequest = bulkRequestBuilder('user')(data);
 console.log(bulkRequest);
 
 // TODO: give the option of deleting the current index or adding to it
